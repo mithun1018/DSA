0794d49e805b161679c4f4c42aa28d8ef8dd5cf5
mithun1018 <143630702+mithun1018@users.noreply.github.com>
2025-05-23 10:17:10 +0530

    Time: 29 ms (4.45%) | Memory: 45.8 MB (44.75%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,32 +1,29 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int zeroMat=0;
-        ArrayList<Integer> arr1=new ArrayList<>();
-        boolean flag=false;
-        ArrayList<Integer> arr=new ArrayList<>();
+        ArrayList<Integer> arri=new ArrayList<>();
+        ArrayList<Integer> arrj=new ArrayList<>();
         for(int i=0;i<matrix.length;i++){
              for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-                    flag=true;
-                    arr.add(j);
-                    arr1.add(i);
+                   
+                    arrj.add(j);
+                    arri.add(i);
                 }
              }
         }
-        if(flag){
             for(int i=0;i<matrix.length;i++){
              for(int j=0;j<matrix[0].length;j++){
-                if(arr1.contains(i)){
+                if(arri.contains(i)){
                     matrix[i][j]=0;
                 }
-                if(arr.contains(j)){
+                if(arrj.contains(j)){
                     matrix[i][j]=0;
                 }
              }
         }
 
 
-        }
+        
         
 
         
