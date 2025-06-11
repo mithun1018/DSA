9f012d79c29cbef9e8645a49a5373174f25488e8
mithun1018 <143630702+mithun1018@users.noreply.github.com>
2025-06-11 14:44:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.java
@@ -0,0 +1,44 @@
+// User function Template for Java
+
+class Solution {
+    public int minTime(int[] arr, int k) {
+        // code here
+        int low=0;
+        int high=0;
+        int mid=0;
+        int ans=0;
+        // if(arr.length<k){
+        //     return -1;
+        // }
+        for(int i:arr){
+            low=Math.max(low,i);
+            high+=i;
+        }
+        while(low<=high){
+            mid=(low+high)/2;
+            if(can(arr,mid)>k){
+                
+                low=mid+1;
+            }else{
+                ans=mid;
+                high=mid-1;
+            }
+        }
+        return low;
+        
+    }
+     public static int can(int[] arr,int mid){
+        int std=1;
+        int page=0;
+        for(int i=0;i<arr.length;i++){
+            if(page+arr[i]<=mid){
+                page+=arr[i];
+            }else{
+                std++;
+                page=arr[i];
+            }
+        }
+        return std;
+       
+    }
+}
