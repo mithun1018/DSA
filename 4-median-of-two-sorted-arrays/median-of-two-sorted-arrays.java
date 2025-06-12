d4927d572cff46fa525d3a2d2b343e23d33e367a
mithun1018 <143630702+mithun1018@users.noreply.github.com>
2025-06-12 12:43:21 +0530

    Time: 1 ms (100.00%) | Memory: 46.3 MB (26.32%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,67 +1,49 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n=nums1.length+nums2.length;
-        int ele1=n/2;
-        int ele2=ele1-1;
-        int val1=0;
-        int val2=0;
-        int point=0;
-        int i=0;
-        int j=0;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]<=nums2[j]){
-                
-                
-                if(point==ele1){
-                val1=nums1[i];
+        int n1=nums1.length;
+        int n2=nums2.length;
+        if(n1>n2){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int n=n1+n2;
+        int left=(n1+n2+1)/2;
+        int low=0;
+        int high=n1;
+        while(low<=high){
+            int mid1=(low+high)/2;
+            int mid2=left-mid1;
+            int l1=Integer.MIN_VALUE;
+            int l2=Integer.MIN_VALUE;
+            int r1=Integer.MAX_VALUE;
+            int r2=Integer.MAX_VALUE;
+            if(mid1>0){
+                l1=nums1[mid1-1];
+            }
+            if(mid2>0){
+                l2=nums2[mid2-1];
+            }
+            if(mid1<n1){
+                r1=nums1[mid1];
+            }
+            if(mid2<n2){
+                r2=nums2[mid2];
+            }
+            if(l1<=r2 && l2<=r1){
+                if(n%2==1){
+                    return (double)Math.max(l1,l2);
+                }else{
+                    return   ((double) (Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
                 }
-                if(point==ele2){
-                val2=nums1[i];
-                }
-                point++;
-                i++;
-            }else{
-                
-                
-                if(point==ele1){
-                val1=nums2[j];
-                }
-                if(point==ele2){
-                val2=nums2[j];
-                }
-                point++;
-                j++;
+            }
+            else if(l1>r2){
+                high=mid1-1;
+            }
+            else{
+                low=mid1+1;
             }
            
         }
-        while(i<nums1.length){
-           
-                if(point==ele1){
-                val1=nums1[i];
-                }
-                if(point==ele2){
-                val2=nums1[i];
-                }
-                 i++;
-            point++;
-
-        }
-        while(j<nums2.length){
-             
-                if(point==ele1){
-                val1=nums2[j];
-                }
-                if(point==ele2){
-                val2=nums2[j];
-                }
-                 j++;
-                point++;
-
-        }
-        if(n%2==0){
-            return (double)(val1+val2)/2;
-        }
-        return (double) val1;
+         return 0;
 
         
     }
