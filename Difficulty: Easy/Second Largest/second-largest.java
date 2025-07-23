31be2c9caebf678d41bb5204a76c8609c12b2e04
mithun1018 <143630702+mithun1018@users.noreply.github.com>
2025-07-23 22:30:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Second Largest/second-largest.java
+++ Difficulty: Easy/Second Largest/second-largest.java
@@ -1,52 +1,18 @@
-//{ Driver Code Starts
-// Initial Template for Java
-import java.io.*;
-import java.util.*;
-import java.util.stream.Collectors;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        int t = Integer.parseInt(sc.nextLine());
-        while (t-- > 0) {
-            String[] arr1Str = sc.nextLine().split(" ");
-            int[] arr = Arrays.stream(arr1Str).mapToInt(Integer::parseInt).toArray();
-            Solution ob = new Solution();
-            int ans = ob.getSecondLargest(arr);
-            System.out.println(ans);
-
-            System.out.println("~");
-        }
-    }
-}
-
-// } Driver Code Ends
-
-
 class Solution {
     public int getSecondLargest(int[] arr) {
         // code here
-         int max1=Integer.MIN_VALUE;
-         int max2=Integer.MIN_VALUE;
-         for(int i=0;i<arr.length;i++){
-             if(arr[i]==max1){
-               continue;
-             }
-             if(arr[i]>max1){
-                 max2=max1;
-                 max1=arr[i];
-             }
-             else {
-                 max2=Math.max(arr[i],max2);
-             }
-         }
-         
-         if(max2==Integer.MIN_VALUE){
-             return -1;
-         }
-         return max2;
-       
+        int max1=0;
+        int max2=0;
+        for(int i:arr){
+            max1=Math.max(max1,i);
+        }
+        for(int i:arr){
+            if(i>max2 && i<max1){
+                max2=i;
+            }
+        }
+        if(max2==0) return -1;
+        else return max2;
         
     }
 }
