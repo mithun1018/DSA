6cdfd6eacb2e4e7c43ed3666aaca07ce44c2bb4c
mithun1018 <143630702+mithun1018@users.noreply.github.com>
2026-01-04 21:53:15 +0530

    Time: 129 ms (5.11%) | Memory: 47.3 MB (13.63%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,25 +1,18 @@
 class Solution {
-        boolean reverse(int i,String s){
-         if(i>=s.length()/2){
-             return true;
-         }
-           if(s.charAt(i)!=s.charAt(s.length()-1-i)){
-            return false;
-           }
-             
-             return reverse(++i,s);
-         
-        
-    }
     public boolean isPalindrome(String s) {
-        String dup=new String();
-        for(int i=0;i<s.length();i++){
-            if(Character.isLetter(s.charAt(i)) || Character.isDigit(s.charAt(i))){
-                dup+=Character.toLowerCase(s.charAt(i));
-            }
-        }
-        return reverse(0,dup);
+        s = s.toLowerCase();
+        char[] arr = s.toCharArray();
 
-        
+        String temp = "";
+        for(int i = 0 ; i < arr.length; i++){
+             if(Character.isDigit(arr[i]) || Character.isLetter(arr[i])){
+                temp+=arr[i];
+             }
+        }
+        StringBuilder sb = new StringBuilder(temp);
+        sb = sb.reverse();
+       if( temp.equals(sb.toString()))  return true;
+       return false;
+       
     }
 }
