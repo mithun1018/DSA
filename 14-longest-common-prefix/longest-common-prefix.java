531bb3371ba6a321660f4153c31b83cd3ba89003
mithun1018 <143630702+mithun1018@users.noreply.github.com>
2026-01-08 22:30:32 +0530

    Time: 5 ms (6.15%) | Memory: 43.5 MB (10.75%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,32 +1,27 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int min=Integer.MAX_VALUE;
-        int id=0;
-        for(int i=0;i<strs.length;i++){
-            if(strs[i].length()<min){
-                min=strs[i].length();
-                id=i;
+        int sLen = Integer.MAX_VALUE;
+        String temp = "";
+       String out = "";
+        for(String s: strs){
+            sLen = Math.min(sLen,s.length());
+            if(s.length() == sLen){
+                temp = s;
             }
-          
         }
-        char[] arr=strs[id].toCharArray();
-        String out="";
-        boolean flag=false;
-        boolean flag1=true;
-
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<strs.length;j++){
-                if(arr[i]!=strs[j].charAt(i)){
-                  flag1=false;
-                }   
+        for(int i = 0 ; i < sLen ; i++){
+            boolean flag = true ;
+           for(int j = 0 ; j < strs.length ; j++){
+            if(strs[j].charAt(i) != temp.charAt(i)){
+               flag = false;
+               return out;
             }
-            System.out.print(flag1);
-            if(flag1){
-                out+=arr[i];
-            }
+           }
+           if(flag){
+            out +=  temp.charAt(i)+"";
+           }
         }
        
         return out;
-        
     }
 }
