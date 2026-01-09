3b48981183e4f7e108dcd11f4ee79148413eb75c
mithun1018 <143630702+mithun1018@users.noreply.github.com>
2026-01-09 22:30:40 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (45.03%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,23 +1,16 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if(haystack.length()<needle.length()){
+        if(!(haystack.contains(needle))){
             return -1;
         }
-        for(int i=0;i<haystack.length();i++){
-            if(haystack.charAt(i)==needle.charAt(0)&& haystack.length()-i>=needle.length()){
-                boolean flag=true;
-                for(int j=0;j<needle.length();j++){
-                    if(haystack.charAt(i+j)!=needle.charAt(j)){
-                        flag=false;
-                    }
-                }
-                if(flag){
+        for(int i = 0 ; i < haystack.length() ; i++){
+            if(haystack.charAt(i) == needle.charAt(0) && i+needle.length() <= haystack.length()){
+                String temp = haystack.substring(i,i+needle.length());
+                if(temp.equals(needle)){
                     return i;
                 }
             }
         }
-        return -1;
-
-        
+        return 0;
     }
 }
