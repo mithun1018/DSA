ff8bc9723618ad607510824a334597102115536e
mithun1018 <143630702+mithun1018@users.noreply.github.com>
2026-01-11 22:30:30 +0530

    Time: 10 ms (18.51%) | Memory: 46.8 MB (17.00%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,12 +1,10 @@
 class Solution {
     public String reverseWords(String s) {
         String[] arr=s.split("\\s+");
-        String out="";
-        for(int i=arr.length-1;i>=0;i--){
-            out+=arr[i]+" ";
+        String out = "";
+        for(int i = arr.length-1 ; i >= 0  ; i--){
+            out += arr[i]+" ";
         }
-        
         return out.trim();
-        
     }
 }
